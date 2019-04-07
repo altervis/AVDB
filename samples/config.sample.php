019c3812f561e7d4755e951a7d279ddbe3b9ab1d
Dmitriy Dankin <dankindi@gmail.com>
2019-04-07 18:14:43 +0300

    Change define policy

--- samples/config.sample.php
+++ samples/config.sample.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_TYPE', 'mysql');
-define('DB_CHARSET', 'utf8mb4');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', '');
+defined('DB_TYPE') or define('DB_TYPE', 'mysql');
+defined('DB_CHARSET') or define('DB_CHARSET', 'utf8mb4');
+defined('DB_HOST') or define('DB_HOST', 'localhost');
+defined('DB_USER') or define('DB_USER', 'root');
+defined('DB_PASS') or define('DB_PASS', '');
+defined('DB_NAME') or define('DB_NAME', '');
